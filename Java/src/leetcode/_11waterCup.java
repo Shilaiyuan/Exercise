94390c034f3dec663e4e216485039e095f877ecc
Laiandming <y12h23@2017.cqut.edu.cn>
2021-04-23 14:37:14 +0800

    2021.4.23
    No.11

--- Java/src/leetcode/_11waterCup.java
+++ Java/src/leetcode/_11waterCup.java
@@ -1,19 +1,41 @@
 package leetcode;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
  * leetcode题号11，盛水最多的容器
- *
+ * 双指针法
  * */
 public class _11waterCup {
-    public int maxAArea(int[] height){
-    return 0;
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int ret = 0;
+        while(i < j){
+//            判断短板，蓄水量等于 间距*短板高度
+            ret = height[i] < height[j] ? Math.max(ret, (j - i) * (height[i++])):Math.max(ret, (j - i) * height[j--]);
+        }
+        return ret;
     }
+
     public static void main(String[] args)throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
+        String str = bf.readLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+           if (str.charAt(i) != '[' && str.charAt(i) != ']'){
+               sb.append(str.charAt(i));
+           }else continue;
+        }
+        String[] str2 = sb.toString().split(",");
+        int[] nums = new int[str2.length];
+        for (int i = 0; i < str2.length; i++){
+            nums[i] = Integer.parseInt(str2[i]);
+        }
+        _11waterCup sol = new _11waterCup();
+        System.out.println(sol.maxArea(nums));
     }
 }
---
+++ Java/src/leetcode/_15_3sum.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * leetcode题号 15 三数之和,找出所有和为0且不重复的三元组
+ *
+ * */
+public class _15_3sum {
+    public List<List<Integer>> threeSum(int[] nums){
+
+    }
+
+    public static void main(String[] args)throws IOException{
+
+    }
+}
